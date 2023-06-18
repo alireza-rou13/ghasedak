d93c64b0f367ccf5a1e3749dce0acb04c1be5895
AliReza Rouhani <alireza.rou13@gmail.com>
2023-06-18 17:42:04 +0330

    added entity type

--- src/city/entities/city.entity.ts
+++ src/city/entities/city.entity.ts
@@ -1,1 +1,9 @@
-export class City {}
+import { ApiProperty } from "@nestjs/swagger";
+import { City } from "@prisma/client";
+
+export class CityEntity implements City {
+    @ApiProperty()
+    id: string;
+    @ApiProperty()
+    title: string;
+}
