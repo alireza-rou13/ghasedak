63dee8f79f31dedbca696e3b7120bf4413af3c30
AliReza Rouhani <alireza.rou13@gmail.com>
2023-06-18 17:42:19 +0330

    added api response documentation

--- src/city/city.controller.ts
+++ src/city/city.controller.ts
@@ -2,7 +2,9 @@ import { Controller, Get, Post, Body, Patch, Param, Delete, NotFoundException } 
 import { CityService } from './city.service';
 import { CreateCityDto } from './dto/create-city.dto';
 import { UpdateCityDto } from './dto/update-city.dto';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiCreatedResponse, ApiTags } from '@nestjs/swagger';
+import { CityEntity } from './entities/city.entity';
+import { isArray } from 'util';
 
 @Controller('city')
 @ApiTags("City")
@@ -10,16 +12,19 @@ export class CityController {
   constructor(private readonly cityService: CityService) {}
 
   @Post()
+  @ApiCreatedResponse({type:CityEntity})
   async create(@Body() createCityDto: CreateCityDto) {
     return await this.cityService.create(createCityDto);
   }
 
   @Get()
+  @ApiCreatedResponse({type:CityEntity, isArray : true})
   async findAll() {
     return await this.cityService.findAll();
   }
 
   @Get(':id')
+  @ApiCreatedResponse({type:CityEntity})
   async findOne(@Param('id') id: string) {
     const city = await this.cityService.findOne(id);
     if(!city){
@@ -29,11 +34,13 @@ export class CityController {
   }
 
   @Patch(':id')
+  @ApiCreatedResponse({type:CityEntity})
   async update(@Param('id') id: string, @Body() updateCityDto: UpdateCityDto) {
     return await this.cityService.update(id, updateCityDto);
   }
 
   @Delete(':id')
+  @ApiCreatedResponse({type:CityEntity})
   remove(@Param('id') id: string) {
     return this.cityService.remove(id);
   }
