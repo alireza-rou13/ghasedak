c035730e7ed2de4cac8da24c055ee869b07cbbd8
AliReza Rouhani <alireza.rou13@gmail.com>
2023-06-18 16:08:49 +0330

    add users and location resource logic

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { BookingModule } from './booking/booking.module';
+import { UsersModule } from './users/users.module';
+import { CityModule } from './city/city.module';
+import { LocationsModule } from './locations/locations.module';
 
 @Module({
-  imports: [PrismaModule, BookingModule],
+  imports: [PrismaModule, BookingModule, UsersModule, CityModule, LocationsModule],
   controllers: [AppController],
   providers: [AppService],
 })
