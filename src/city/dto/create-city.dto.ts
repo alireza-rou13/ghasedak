59caa2a5d59f9a64d927e2b297df83d0ecbd8c44
AliReza Rouhani <alireza.rou13@gmail.com>
2023-06-19 08:52:20 +0330

    added class validtor

--- src/city/dto/create-city.dto.ts
+++ src/city/dto/create-city.dto.ts
@@ -1,6 +1,9 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { MinLength, IsString } from 'class-validator';
 
 export class CreateCityDto {
   @ApiProperty()
+  @IsString()
+  @MinLength(2)
   title: string;
 }
