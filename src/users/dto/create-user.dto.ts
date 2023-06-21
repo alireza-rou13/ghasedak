d7f8f82055d0388bcbbab3d258273fa62494ea1e
AliReza Rouhani <alireza.rou13@gmail.com>
2023-06-21 15:17:08 +0330

    create dto for users

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,31 @@
-export class CreateUserDto {}
+import { ApiProperty } from "@nestjs/swagger";
+import { Prisma } from "@prisma/client";
+import { IsOptional, IsString, MinLength } from "class-validator";
+
+export class CreateUserDto {
+  @ApiProperty()
+  @IsString()
+  @MinLength(3)
+  username: string
+
+  @ApiProperty()
+  @IsString()
+  @IsOptional()
+  @MinLength(3)
+  fullname?: string
+
+  @ApiProperty()
+  @IsString()
+  @IsOptional()
+  @MinLength(3)
+  hash?: string
+
+  @ApiProperty()
+  @IsString()
+  @IsOptional()
+  @MinLength(2)
+  data?: Prisma.InputJsonObject
+
+
+
+}
