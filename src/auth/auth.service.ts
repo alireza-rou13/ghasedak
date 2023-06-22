0dde6a070c63e11dddf12cb13eb04b43c3555cf0
AliReza Rouhani <alireza.rou13@gmail.com>
2023-06-22 12:24:10 +0330

    add JWT Token to response singin User

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,27 +1,41 @@
 import { ForbiddenException, Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { LoginDto } from './dto/login.dto';
 import { SignupDto } from './dto/signup.dto';
 import * as argon from 'argon2';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AuthService {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    private prisma: PrismaService,
+    private jwtService: JwtService,
+    private config: ConfigService,
+  ) {}
 
   async login(loginDto: LoginDto) {
     //find User by username
-    const user = await this.prisma.user.findUniqueOrThrow({where: {username: loginDto.username}})
+    const user = await this.prisma.user.findUniqueOrThrow({
+      where: { username: loginDto.username },
+    });
     //throw error if not found
     //compare password
-    const pwmatches = await argon.verify(user.hash,loginDto.password);
+    const pwmatches = await argon.verify(user.hash, loginDto.password);
     //throw error if password not equal
-    if(!pwmatches)
-      throw new ForbiddenException('Credentials Incorrect')
+    if (!pwmatches) throw new ForbiddenException('Credentials Incorrect');
     //send back to user
-    return user;
-    
-    return 'login';
+    const payload = { sub: user.id, username: user.username };
+    const secret = this.config.get('JWT_SECRET');
+    //return Token
+    return {
+      access_token: await this.jwtService.signAsync(payload, {
+        expiresIn: '240m',
+        secret: secret,
+      }),
+    };
   }
+
   async signup(signupDto: SignupDto) {
     //Generate Password Hash
     const hash = await argon.hash(signupDto.password);
