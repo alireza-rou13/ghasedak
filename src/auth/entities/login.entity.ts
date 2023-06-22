8473963f93365aa9717165c8b317d97f1fd6cd86
AliReza Rouhani <alireza.rou13@gmail.com>
2023-06-22 10:46:53 +0330

    init Login Entity

---
+++ src/auth/entities/login.entity.ts
@@ -0,0 +1,22 @@
+import { Prisma, Role, User } from '@prisma/client';
+import { ApiProperty } from '@nestjs/swagger';
+
+export class LoginEntity implements User {
+  @ApiProperty()
+  id: string;
+  @ApiProperty()
+  username: string;
+  @ApiProperty()
+  fullname: string;
+  @ApiProperty()
+  hash: string;
+  @ApiProperty()
+  role: Role;
+  @ApiProperty()
+  cityId: string;
+  @ApiProperty()
+  data: Prisma.JsonValue;
+  @ApiProperty()
+  status: boolean;
+
+}
