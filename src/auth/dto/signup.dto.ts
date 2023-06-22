7e6cd855734b771bca7b087134ca26d1333a60d5
AliReza Rouhani <alireza.rou13@gmail.com>
2023-06-22 10:46:39 +0330

    create SignupDto

---
+++ src/auth/dto/signup.dto.ts
@@ -0,0 +1,21 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { IsOptional, IsString, MinLength } from "class-validator";
+
+export class SignupDto {
+  @IsString()
+  @MinLength(2)
+  @ApiProperty()
+  username:string
+
+  @IsString()
+  @MinLength(2)
+  @ApiProperty()
+  password:string
+
+  @IsString()
+  @IsOptional()
+  @MinLength(3)
+  @ApiProperty()
+  fullname?:string
+
+}
