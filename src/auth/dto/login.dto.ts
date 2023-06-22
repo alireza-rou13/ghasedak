2495f4920af46516c0e4f13d864a3ab58a239e76
AliReza Rouhani <alireza.rou13@gmail.com>
2023-06-22 10:18:01 +0330

    create login DTO

---
+++ src/auth/dto/login.dto.ts
@@ -0,0 +1,14 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { IsString, MinLength } from "class-validator";
+
+export class LoginDto {
+  @IsString()
+  @MinLength(2)
+  @ApiProperty()
+  username:string
+
+  @IsString()
+  @MinLength(2)
+  @ApiProperty()
+  password:string
+}
