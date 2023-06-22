baf3d3d0717f93c43f4b2c1ec34a5f6b967125d1
AliReza Rouhani <alireza.rou13@gmail.com>
2023-06-22 11:02:35 +0330

    added Login Service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { LoginDto } from './dto/login.dto';
 import { SignupDto } from './dto/signup.dto';
@@ -7,24 +7,33 @@ import * as argon from 'argon2';
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
-  
-  async login(loginDto: LoginDto){
-    //Generate Password Hash
 
-
+  async login(loginDto: LoginDto) {
+    //find User by username
+    const user = await this.prisma.user.findUniqueOrThrow({where: {username: loginDto.username}})
+    //throw error if not found
+    //compare password
+    const pwmatches = await argon.verify(user.hash,loginDto.password);
+    //throw error if password not equal
+    if(!pwmatches)
+      throw new ForbiddenException('Credentials Incorrect')
+    //send back to user
+    return user;
+    
     return 'login';
   }
   async signup(signupDto: SignupDto) {
-    
     //Generate Password Hash
-    const hash=await argon.hash(signupDto.password)
+    const hash = await argon.hash(signupDto.password);
     //save New user in DB
-    const user = await this.prisma.user.create({data:{
-      username:signupDto.username,
-      hash,
-      fullname:signupDto.fullname,
-    }})
+    const user = await this.prisma.user.create({
+      data: {
+        username: signupDto.username,
+        hash,
+        fullname: signupDto.fullname,
+      },
+    });
     //Return User is created
-    return {msg : "user created"}
+    return { msg: 'user created' };
   }
 }
