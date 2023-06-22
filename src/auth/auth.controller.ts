d1ae2a4fae1a69e75ce17d55dd2c8250d3860851
AliReza Rouhani <alireza.rou13@gmail.com>
2023-06-22 10:18:13 +0330

    add Post Login route

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { ApiCreatedResponse, ApiTags } from '@nestjs/swagger';
+import { AuthService } from './auth.service';
+import { LoginDto } from './dto/login.dto';
 
 @Controller('auth')
-export class AuthController {}
+@ApiTags('Authentication')
+export class AuthController {
+  constructor(private readonly authService: AuthService){}
+  
+  @Post('login')
+  // @ApiCreatedResponse
+  async signin(@Body() loginDto: LoginDto){
+    return await this.authService.login(loginDto);
+  }
+}
