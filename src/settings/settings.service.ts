d3a03113d2d3b4cbd0f32c5f1cd71685db7991c5
AliReza Rouhani <alireza.rou13@gmail.com>
2023-06-25 11:55:26 +0330

    init Create func

--- src/settings/settings.service.ts
+++ src/settings/settings.service.ts
@@ -1,26 +1,28 @@
 import { Injectable } from '@nestjs/common';
 import { CreateSettingDto } from './dto/create-setting.dto';
 import { UpdateSettingDto } from './dto/update-setting.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class SettingsService {
-  create(createSettingDto: CreateSettingDto) {
-    return 'This action adds a new setting';
+  constructor(private prisma: PrismaService) {}
+  async create(createSettingDto: CreateSettingDto) {
+    return this.prisma.setting.create({data: createSettingDto})
   }
 
-  findAll() {
+  async findAll() {
     return `This action returns all settings`;
   }
 
-  findOne(id: number) {
+  async findOne(id: number) {
     return `This action returns a #${id} setting`;
   }
 
-  update(id: number, updateSettingDto: UpdateSettingDto) {
+  async update(id: number, updateSettingDto: UpdateSettingDto) {
     return `This action updates a #${id} setting`;
   }
 
-  remove(id: number) {
+  async remove(id: number) {
     return `This action removes a #${id} setting`;
   }
 }
