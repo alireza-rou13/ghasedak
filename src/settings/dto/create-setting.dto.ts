f0d90b131b97c0177446ed52df9bdb6bd0002e60
AliReza Rouhani <alireza.rou13@gmail.com>
2023-06-25 11:21:16 +0330

    create dto

--- src/settings/dto/create-setting.dto.ts
+++ src/settings/dto/create-setting.dto.ts
@@ -1,1 +1,27 @@
-export class CreateSettingDto {}
+import { ApiProperty } from "@nestjs/swagger";
+import { Prisma } from "@prisma/client";
+import { IsOptional, IsString, MinLength } from "class-validator";
+
+export class CreateSettingDto {
+  @ApiProperty()
+  @IsString()
+  @MinLength(2)
+  title: string;
+
+  @ApiProperty()
+  @IsString()
+  @MinLength(2)
+  key: string
+
+  @ApiProperty()
+  @IsString()
+  @IsOptional()
+  @MinLength(2)
+  status:boolean
+
+  @ApiProperty()
+  @IsString()
+  @IsOptional()
+  @MinLength(2)
+  options: Prisma.JsonValue;
+}
