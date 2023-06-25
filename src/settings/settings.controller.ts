7f008a226dcba59616ffb6f060131f6788e6e1f6
AliReza Rouhani <alireza.rou13@gmail.com>
2023-06-25 11:07:26 +0330

    add async , await

--- src/settings/settings.controller.ts
+++ src/settings/settings.controller.ts
@@ -16,27 +16,27 @@ export class SettingsController {
   constructor(private readonly settingsService: SettingsService) {}
 
   @Post()
-  create(@Body() createSettingDto: CreateSettingDto) {
-    return this.settingsService.create(createSettingDto);
+  async create(@Body() createSettingDto: CreateSettingDto) {
+    return await this.settingsService.create(createSettingDto);
   }
 
   @Get()
-  findAll() {
-    return this.settingsService.findAll();
+  async findAll() {
+    return await this.settingsService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.settingsService.findOne(+id);
+  async findOne(@Param('id') id: string) {
+    return await this.settingsService.findOne(+id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateSettingDto: UpdateSettingDto) {
-    return this.settingsService.update(+id, updateSettingDto);
+  async update(@Param('id') id: string, @Body() updateSettingDto: UpdateSettingDto) {
+    return await this.settingsService.update(+id, updateSettingDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.settingsService.remove(+id);
+  async remove(@Param('id') id: string) {
+    return await this.settingsService.remove(+id);
   }
 }
