3286d08eb391bc420d5ae6d7bf27626be1c6879b
AliReza Rouhani <alireza.rou13@gmail.com>
2023-06-27 13:12:11 +0330

    return authenticate with active directory

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,12 @@
 import { ForbiddenException, Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
+import * as argon from 'argon2';
+import { ConfigService } from '@nestjs/config';
+import ActiveDirectory = require('activedirectory2');
 import { PrismaService } from 'src/prisma/prisma.service';
 import { LoginDto } from './dto/login.dto';
 import { SignupDto } from './dto/signup.dto';
-import * as argon from 'argon2';
-import { ConfigService } from '@nestjs/config';
+import { use } from 'passport';
 
 @Injectable()
 export class AuthService {
@@ -53,12 +55,30 @@ export class AuthService {
 
   //Login With Active Directory
   async loginAD(loginDto: LoginDto) {
+    const query = await this.prisma.setting.findUnique({
+      where: { key: 'ad_login' },
+      select: { options: true },
+    });
+    const config = {
+      url: query.options['url'],
+      baseDN: query.options['baseDN'],
+      username: loginDto.username,
+      password: loginDto.password,
+    };
     //check if username exists in baseDN
-    //throw error if not found
-    //compare password
-    //throw error if password not equal
-    // if (!pwmatches) throw new ForbiddenException('Credentials Incorrect');
+    // console.log(url.options['url']);
+
+    const username = loginDto.username;
+    const password = loginDto.password;
+
+    const auth = await ad_login(config, username, password);
+    if (!auth) {
+      throw new ForbiddenException('Credentials Incorrect')
+    }
+    // const exists = await this.prisma.user.findUnique('')
     //check if user exists in Ghasedak
+
+
     //create user
     //send back to user
     // const payload = { sub: user.id, username: user.username };
@@ -70,6 +90,23 @@ export class AuthService {
     //     secret: secret,
     //   }),
     // };
-    return 'to do';
   }
+}
+//Login with Active Directory
+async function ad_login(config, username, password) {
+  const ad = new ActiveDirectory(config);
+  return new Promise((resolve, reject) => {
+    ad.authenticate(username, password, (err, auth) => {
+      if (err) {
+        console.log('Authentication failed:', err);
+        reject(new ForbiddenException('Credentials Incorrect'));
+      } else if (auth) {
+        console.log('Authentication successful');
+        resolve('Authentication successful');
+      } else {
+        console.log('Authentication failed: incorrect username or password');
+        reject(new ForbiddenException('Credentials Incorrect'));
+      }
+    });
+  });
 }
