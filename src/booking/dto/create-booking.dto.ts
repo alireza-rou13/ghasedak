3635c5d9feb4282c0d1b626b492cf90f63324f6f
AliReza Rouhani <alireza.rou13@gmail.com>
2023-07-03 13:41:13 +0330

    dto init

--- src/booking/dto/create-booking.dto.ts
+++ src/booking/dto/create-booking.dto.ts
@@ -1,1 +1,23 @@
-export class CreateBookingDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsNumber,
+  IsOptional,
+  IsString,
+  IsUUID,
+  MinLength,
+} from 'class-validator';
+
+export class CreateBookingDto {
+  @IsString()
+  @MinLength(2)
+  @ApiProperty()
+  title: string;
+
+  @IsOptional()
+  @ApiProperty()
+  description: string;
+
+  @IsNumber()
+  @ApiProperty()
+  startDate: number;
+}
