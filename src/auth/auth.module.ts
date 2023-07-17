289a4f451b00f89d2daf3b9d43e8d4972dd502ef
AliReza Rouhani <alireza.rou13@gmail.com>
2023-07-17 07:58:07 +0330

    update jwt strategy

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,10 +4,11 @@ import { AuthService } from './auth.service';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule } from '@nestjs/config';
+import { JwtStrategy } from './strategy';
 
 @Module({
-  controllers: [AuthController],
-  providers: [AuthService],
   imports: [JwtModule.register({}), PrismaModule, ConfigModule],
+  controllers: [AuthController],
+  providers: [AuthService, JwtStrategy],
 })
 export class AuthModule {}
